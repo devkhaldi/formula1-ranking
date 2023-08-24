0f46e1fb4fcf3b307a2379380442490517e3614b
elkhaldi <dev.khaldi@gmail.com>
2023-08-24 17:30:46 +0100

    rankings structure

--- src/components/Rankings.jsx
+++ src/components/Rankings.jsx
@@ -1,7 +1,47 @@
 import React from "react"
 
 const Rankings = () => {
-  return <div>Rankings</div>
+  return (
+    <div>
+      <div>
+        <h1>Rankings</h1>
+        <div>
+          <span>Teams</span>
+          <button>
+            <div></div>
+          </button>
+        </div>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>Position</th>
+            <th>Name</th>
+            <th>Logo</th>
+            <th>Points</th>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default Rankings
