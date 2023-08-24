a0aad273616cbf161850cb28fdb396f2ddcdb818
elkhaldi <dev.khaldi@gmail.com>
2023-08-24 18:15:06 +0100

    hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,14 @@ import img from "../img/image 3.png"
 const Header = () => {
   return (
     <div className='flex bg-[#2D2D2D] text-white'>
-      <div>
-        <span>Formula 1</span>
-        <span>Rankings</span>
+      <div className='py-32 pl-36 text-[116px] leading-[110%]'>
+        <h1>Formula 1 Rankings</h1>
       </div>
-      <div>
-        <img src={img} alt='img 3' />
+      <div className='relative overflow-hidden'>
+        <div className='-top-60 left-20 rounded-full h-[200%] w-[200%] bg-transparent absolute z-20 border-white border-[10px]'></div>
+        <div className='-top-60 left-52 rounded-full h-[200%] w-[200%] bg-transparent absolute z-20 border-[#f00] border-[10px]'></div>
+        <div className='hero-img-container-layer z-10 h-full w-[150%] absolute top-0 left-0'></div>
+        <img src={img} className='object-cover h-full' alt='img 3' />
       </div>
     </div>
   )
